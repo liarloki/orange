1ac9716d2a1a7e255af41ae4ee8b26ec60998638
liar_loki <65863334+liarloki@users.noreply.github.com>
2022-03-17 16:49:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// let butt = document.getElementById("second_hide");
+// let none = "display: none";
+// let see = "display: block";
+ function first() {
+//   if ("second_hide" == "display: none") {
+    document
+      .getElementById("second_hide")
+      .setAttribute("style", "display: block");
+  // } else {
+  //   document
+  //     .getElementById("second_hide")
+  //     .setAttribute("style", "display: none");
+  // }
+  //   document
+  //     .getElementById("second_hide")
+  //     .setAttribute("style", "display: block");
+  //   document.getElementById("second_hide").setAttribute("style", "display: none");
+}
